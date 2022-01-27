442909951e2851ba2c43f66a0352eefe548a3d75
Berkay Eren Konuk <berkayeren2009@gmail.com>
2022-01-27 15:35:51 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/Header';
+
 function App() {
   return (
-    <div>
-      <p>Hello!</p>
+    <div className="App">
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <div className="App-header">
+            <img 
+            class="pp_logo" 
+            src="https://avatars.githubusercontent.com/u/18515671?v=4" 
+            alt="Berikai Profile Picture" />
+            <div className="username">Berikai</div>
+        </div>
+    )
+  }
+  
+export default Header;
