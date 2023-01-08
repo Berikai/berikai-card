525846ace13aa12ed97badd4e7b04aea65522869
Berikai <berkayeren2009@gmail.com>
2023-01-08 19:07:58 +0300

    profile pic resize > smaller

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 function Header() {
     return (
         <div className="App-header">
-            <a href="https://github.com/Berikai">
+            <p><a href="https://github.com/Berikai">
                 <img 
                 className="pp_logo" 
                 src="https://avatars.githubusercontent.com/u/18515671?v=4" 
                 alt="Berikai Profile Picture" />
-            </a>
+            </a></p>
             <div className="username">Berikai<div className="aka">also known as Verdant</div></div>
         </div>
     )
