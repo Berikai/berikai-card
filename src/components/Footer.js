cc57aaad16c67e0321c848f9a34a37fe15463a6c
Berikai <berkayeren2009@gmail.com>
2023-01-08 20:59:24 +0300

    added spotify widget

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 function Footer() {
     return (
         <footer>
+           <iframe style={{borderRadius: '12px', align: 'center'}} src="https://open.spotify.com/embed/track/7BuojdYclDrgYXkiUOyBzx?utm_source=generator&theme=0" width="320" height="100" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
            <p>Made with ❤️ by Berikai </p>
         </footer>
     )
