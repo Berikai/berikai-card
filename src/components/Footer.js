4c87df2efc2b37e7d66dc6365e6cc69cd3c7b3bd
Berikai <berkayeren2009@gmail.com>
2023-01-08 21:44:59 +0300

    https correction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer>
            {/*<iframe style={{borderRadius: '12px', align: 'center'}} src="https://open.spotify.com/embed/track/7BuojdYclDrgYXkiUOyBzx?utm_source=generator&theme=0" width="320" height="100" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>*/}
-           <iframe width="320" height="100" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=https://soundcloud.com/verdantbass/skrillex-kliptown-empyrean-verdant-dnb-flip&color7FFFD4&show_artwork=false&auto_play=true"></iframe>
+           <iframe width="320" height="100" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/verdantbass/skrillex-kliptown-empyrean-verdant-dnb-flip&color7FFFD4&show_artwork=false&auto_play=true"></iframe>
            <p>Made with ❤️ by Berikai </p>
         </footer>
     )
