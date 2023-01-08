c2ad77b3599aeb351586fabcf3f9ca751ac12280
Berikai <berkayeren2009@gmail.com>
2023-01-08 19:40:44 +0300

    Some improvements

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # berikai.github.io
 Homepage website repo for my github profile powered by React.
 
-![Screenshot](https://user-images.githubusercontent.com/18515671/211204666-770eefbd-ff8d-44f3-a4ac-827337aae0b5.png)
-
 ## Fun fact
 As you may notice, I try to experiment a little weird approach. I directly imported React into index.html and all the other components. Experimenting is always fun!
--- src/components/About.js
+++ src/components/About.js
@@ -10,11 +10,11 @@ function About() {
                 <li>🎵 &nbsp; Dubstep, Drum 'n Bass, Experimental...</li>
             </ul>
             <p className="socials" align="left">
-                <a className="margin-inline2" href="https://soundcloud.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://www.svgrepo.com/show/35013/soundcloud.svg" width="32" height="32" /></a>
-                <a className="margin-inline2" href="https://discord.com/users/Verdant#6747" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/discord.svg" width="32" height="32" /></a>
-                <a className="margin-inline2" href="https://www.github.com/Berikai" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/github.svg" width="32" height="32" /></a>
-                <a className="margin-inline2" href="http://www.instagram.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/instagram.svg" width="32" height="32" /></a>
-                <a className="margin-inline2" href="https://www.twitter.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/twitter.svg" width="32" height="32" /></a>
+                <a className="margin-inline2" href="https://soundcloud.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://www.svgrepo.com/show/35013/soundcloud.svg" width="24" height="24" /></a>
+                <a className="margin-inline2" href="https://discord.com/users/Verdant#6747" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/discord.svg" width="24" height="24" /></a>
+                <a className="margin-inline2" href="https://www.github.com/Berikai" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/github.svg" width="24" height="24" /></a>
+                <a className="margin-inline2" href="http://www.instagram.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/instagram.svg" width="24" height="24" /></a>
+                <a className="margin-inline2" href="https://www.twitter.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/twitter.svg" width="24" height="24" /></a>
             </p>
         </div>
     )
