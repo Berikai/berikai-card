3a3f2e245dab74945efbcc11916643f36e45f43f
Berikai <berkayeren2009@gmail.com>
2025-01-13 04:53:25 +0300

    Add heart tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'preact/compat';
 export default function Footer() {
     return (
         <footer className="App-footer">
-            <p className="p-footer">Made with ❤️ by Berikai </p>
+            <p className="p-footer">Made with <heart>❤️</heart> by Berikai </p>
         </footer>
     )
 }
