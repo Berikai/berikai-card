534a8d0eb2ec720e60a8c2ed838829022584bdec
Berikai <berkayeren2009@gmail.com>
2025-01-13 04:54:00 +0300

    Change margin to padding

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,11 +10,11 @@ export default function About() {
                 <li className="li-element">🎵 &nbsp; Dubstep, Drum 'n Bass, Experimental...</li>
             </ul>
             <p className="socials" align="left">
-                <a className="margin-inline" href="https://soundcloud.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://www.svgrepo.com/show/35013/soundcloud.svg" width="24" height="24" alt="Soundcloud"/></a>
-                <a className="margin-inline" href="https://discord.com/users/Verdant#6747" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/discord.svg" width="24" height="24" alt="Discord"/></a>
-                <a className="margin-inline" href="https://www.github.com/Berikai" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/github.svg" width="24" height="24" alt="Github"/></a>
-                <a className="margin-inline" href="http://www.instagram.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/instagram.svg" width="24" height="24" alt="Instagram"/></a>
-                <a className="margin-inline" href="https://www.twitter.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/twitter.svg" width="24" height="24" alt="Twitter"/></a>
+                <a className="padding-inline" href="https://soundcloud.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://www.svgrepo.com/show/35013/soundcloud.svg" width="24" height="24" alt="Soundcloud"/></a>
+                <a className="padding-inline" href="https://discordapp.com/users/385498463745343490" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/discord.svg" width="24" height="24" alt="Discord"/></a>
+                <a className="padding-inline" href="https://www.github.com/Berikai" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/github.svg" width="24" height="24" alt="Github"/></a>
+                <a className="padding-inline" href="http://www.instagram.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/instagram.svg" width="24" height="24" alt="Instagram"/></a>
+                <a className="padding-inline" href="https://www.twitter.com/verdantbass" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/twitter.svg" width="24" height="24" alt="Twitter"/></a>
             </p>
         </div>
     )
