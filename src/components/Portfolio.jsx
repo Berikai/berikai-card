faa596658086c2a063309ed6ccfc58ebe552fa14
Berikai <berkayeren2009@gmail.com>
2025-01-23 02:16:10 +0300

    Add portfolio photos to repo

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,10 @@
 import React from 'preact/compat';
 
+import portfolioBTE from '../assets/img/portfolio_bte.png';
+import portfolioBTEW from '../assets/img/portfolio_btew.png';
+import portfolioPixbud from '../assets/img/portfolio_pixbud.gif';
+import portfolioOasis from '../assets/img/portfolio_oasis.png';
+
 export default function Portfolio() {
     return (
         <div className="App-portfolio" style={"opacity: 0;transition: transform 0.1s;"}>
@@ -11,7 +16,7 @@ export default function Portfolio() {
                 <div className="portfolio-element">
                     <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
                     href="https://github.com/Berikai/bitwig-theme-editor"></a>
-                    <img className="portfolio-img" width={68} src="https://cdn.discordapp.com/attachments/935545276008116276/1331268052716752896/image.png?ex=6790ff7d&is=678fadfd&hm=2feaee3fb40c09aa0e2bceb14b9fd58ead48fe9f0b8bc11667436b2729322335&"></img>
+                    <img className="portfolio-img" width={68} src={portfolioBTE} alt="Bitwig Theme Editor"></img>
                     <div className="portfolio-element-content dark">
                         <div className="portfolio-element-title dark">Bitwig Theme Editor</div>
                         <div className="portfolio-element-info dark">A patcher for Bitwig Studio, let's you customize the appearance of the most modular DAW in the world!</div>
@@ -24,7 +29,7 @@ export default function Portfolio() {
                         <div className="portfolio-element-title dark">Bitwig Theme Editor WebUI</div>
                         <div className="portfolio-element-info dark">Interactive visual playground to create and edit themes with ease and joy for Bitwig Studio</div>
                     </div>
-                    <img className="portfolio-img" width={68} src="https://cdn.discordapp.com/attachments/935545276008116276/1331267497726316647/image.png?ex=6790fef9&is=678fad79&hm=fe4a49a3ae9a4563eb2a050e095a73bf51ac0d14812c3958e64973c736c3f5f7&"></img>
+                    <img className="portfolio-img" width={68} src={portfolioBTEW} alt="Bitwig Theme Editor WebUI"></img>
                 </div>
                 <div className="portfolio-element">
                     <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
@@ -35,7 +40,7 @@ export default function Portfolio() {
                             window.open(e.target.href, '_blank');
                         }
                     }}></a>
-                    <img className="portfolio-img" width={68} src="https://cdn.discordapp.com/attachments/935545276008116276/1331266107226456086/ezgif-2-6735a308d0.gif?ex=6790fdad&is=678fac2d&hm=3532848315807f625c44ab7912f6d942c7daf1028d53abe87b09e61c92ba1ef1&"></img>
+                    <img className="portfolio-img" width={68} src={portfolioPixbud} alt="Pixbud"></img>
                     <div className="portfolio-element-content dark">
                         <div className="portfolio-element-title dark">Pixbud</div>
                         <div className="portfolio-element-info dark">Your desktop buddy, loves to hang around on the rooftops of your application windows</div>
@@ -48,7 +53,7 @@ export default function Portfolio() {
                         <div className="portfolio-element-title dark">OASIS Android</div>
                         <div className="portfolio-element-info dark">An unofficial mobile application for the student information system of Izmir University of Economics</div>
                     </div>
-                    <img className="portfolio-img" width={68} src="https://cdn.discordapp.com/attachments/935545276008116276/1331268275077644370/image.png?ex=6790ffb2&is=678fae32&hm=4f1108b77f83c9e40fa3e1f4eb4f809585c2b7d1db8ddeeb4835a0798ba8d593&"></img>
+                    <img className="portfolio-img" width={68} src={portfolioOasis} alt="OASIS Android"></img>
                 </div>
                 <div className="portfolio-element">
                     <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
